b722696ec79c9201ffd861615211de49a304b44e
Haidar Alessa <10682@holbertonstudents.com>
2025-07-28 06:37:00 +0000

    use List of typing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+
+from typing import List
+
 """
 Complex annotations - list of floats
 """
 
 
-def sum_list(numbers: list[float]) -> float:
+def sum_list(numbers: List[float]) -> float:
     """function accepts a list of float numbers and returns sum of them"""
     return float(sum(numbers))
