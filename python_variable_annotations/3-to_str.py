7dbfaddd11b54bd10b7200fef95e683c46700a99
Haidar Alessa <10682@holbertonstudents.com>
2025-07-28 06:12:07 +0000

    to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """function accepts a float number and returns string value of it"""
+    return str(n)
