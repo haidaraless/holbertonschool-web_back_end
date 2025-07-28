080572ae0fa4ea55a02f940b1f50bcdf585334e3
Haidar Alessa <10682@holbertonstudents.com>
2025-07-28 06:07:46 +0000

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - floor
+"""
+
+
+def floor(n: float) -> int:
+    """function accepts a float number and returns integer value of it"""
+    return int(n)
