bd452843e581927dfb8bcaa12e1db37c80d06b4c
Haidar Alessa <10682@holbertonstudents.com>
2025-07-28 06:44:31 +0000

    use typing.Union

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -2,10 +2,10 @@
 """
 Complex annotations - list of mixed floats and integers
 """
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[float | int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """function accepts a list of mixed float and int numbers
     and returns sum of them"""
     return float(sum(mxd_lst))
