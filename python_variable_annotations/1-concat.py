8f52b9c966cb62bf500fc8c6e73b442547fc88f4
Haidar Alessa <10682@holbertonstudents.com>
2025-07-28 05:16:54 +0000

    concatenation

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - concat strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """function accepts two strings and returns concatenation of type string"""
+    return str(str1 + str2)
