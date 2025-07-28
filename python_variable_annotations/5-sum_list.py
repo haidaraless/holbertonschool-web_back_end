398c0e3939bb81816f7fec7684b3edaaf86fc74d
Haidar Alessa <10682@holbertonstudents.com>
2025-07-28 06:32:22 +0000

    list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Complex annotations - list of floats
+"""
+
+
+def sum_list(numbers: list[float]) -> float:
+    """function accepts a list of float numbers and returns sum of them"""
+    return sum(numbers)
