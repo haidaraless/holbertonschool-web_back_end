3756034042039b29cada51bae1ff6eff2d259223
Haidar Alessa <10682@holbertonstudents.com>
2025-08-17 20:34:05 +0000

    fix a typo

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -33,7 +33,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = 1, page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Deletion handlation without missing data"""
 
         assert isinstance(index, int) and index >= 0
