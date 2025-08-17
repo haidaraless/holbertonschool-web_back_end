c4b27b07e5f49cab9e21f0ad92425ef7f03d111c
Haidar Alessa <10682@holbertonstudents.com>
2025-08-17 18:19:23 +0000

    fix 2-hypermedia

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -16,8 +16,7 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -42,13 +41,14 @@ class Server:
         return list(data[start:end])
     
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict():
-        data = self.get_page(page, page_size)
+        data_page = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
 
         return {
-            "page_size": len(data),
+            "page_size": len(data_page),
             "page": page,
-            "data": data,
-            "next_page": None if (page + 1 >= len(data)) else (page + 1), # need fix
-            "prev_page": None if (page - 1 < 0) else (page - 1),
-            "total_pages": 0 # need solve
+            "data": data_page,
+            "next_page": (page + 1) if page < total_pages else None,
+            "prev_page": (page - 1) if page > 1 else None,
+            "total_pages": total_pages
         }
